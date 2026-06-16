790362167377320be1b5616bd96871e874ae56ac
/run/claude <284144606+oddsclaude@users.noreply.github.com>
2026-06-16 15:55:50 -0400

    Add platform_win32.c and platform_sdl2.c (forgot these in previous commit)

---
+++ platform_sdl2.c
@@ -0,0 +1,255 @@
+/*
+ * platform_sdl2.c - SDL2 renderer for ProgressBar95
+ * Cross-platform: Linux/Wayland, Windows (Win98+), macOS
+ *
+ * Compile: gcc -o progressbar95 platform_sdl2.c game.c -lSDL2 -lSDL2_ttf
+ * Wayland: SDL_VIDEODRIVER=wayland ./progressbar95
+ * Needs: libsdl2-dev libsdl2-ttf-dev (apt) / sdl2 sdl2_ttf (pacman)
+ */
+
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_ttf.h>
+#include <stdio.h>
+#include <string.h>
+#include "game.h"
+
+#define WIN_W  AREA_W
+#define WIN_H  (AREA_H + 22)
+#define TICK_MS 40
+
+static SDL_Window   *g_win;
+static SDL_Renderer *g_ren;
+static TTF_Font     *g_font;
+static TTF_Font     *g_fontLg;
+
+/* ---- color helpers ---- */
+
+typedef struct { Uint8 r, g, b; } Col;
+
+static Col dot_col(DotKind k) {
+    switch (k) {
+        case DOT_BLUE:   return (Col){0,80,220};
+        case DOT_ORANGE: return (Col){255,140,0};
+        case DOT_PINK:   return (Col){255,50,150};
+        case DOT_GREY:   return (Col){150,150,150};
+        case DOT_RED:    return (Col){200,0,0};
+        case DOT_RANDOM: return (Col){180,0,200};
+        case DOT_GREEN:  return (Col){0,180,0};
+        default:         return (Col){255,255,255};
+    }
+}
+
+static Col cycle_col(void) {
+    static Col c[] = {
+        {0,80,220},{255,140,0},{255,50,150},{150,150,150},{200,0,0},{0,180,0}
+    };
+    return c[(g_animTick/6)%6];
+}
+
+/* ---- text rendering ---- */
+
+static void rtext(TTF_Font *f, int x, int y, const char *s, Uint8 r, Uint8 g, Uint8 b) {
+    SDL_Color col = {r,g,b,255};
+    SDL_Surface *sur;
+    SDL_Texture *tex;
+    SDL_Rect dst;
+    if (!f || !s || !s[0]) return;
+    sur = TTF_RenderText_Solid(f, s, col);
+    if (!sur) return;
+    tex = SDL_CreateTextureFromSurface(g_ren, sur);
+    dst.x=x; dst.y=y; dst.w=sur->w; dst.h=sur->h;
+    SDL_RenderCopy(g_ren, tex, NULL, &dst);
+    SDL_DestroyTexture(tex);
+    SDL_FreeSurface(sur);
+}
+
+static void rtxt(int x, int y, const char *s, Uint8 r, Uint8 g, Uint8 b) {
+    rtext(g_font, x, y, s, r, g, b);
+}
+
+static void rtxt_lg(int x, int y, const char *s, Uint8 r, Uint8 g, Uint8 b) {
+    rtext(g_fontLg ? g_fontLg : g_font, x, y, s, r, g, b);
+}
+
+/* ---- draw helpers ---- */
+
+static void fill(int x, int y, int w, int h, Uint8 r, Uint8 g, Uint8 b) {
+    SDL_Rect rc = {x,y,w,h};
+    SDL_SetRenderDrawColor(g_ren,r,g,b,255);
+    SDL_RenderFillRect(g_ren,&rc);
+}
+
+static void outline(int x, int y, int w, int h, Uint8 r, Uint8 g, Uint8 b) {
+    SDL_Rect rc = {x,y,w,h};
+    SDL_SetRenderDrawColor(g_ren,r,g,b,255);
+    SDL_RenderDrawRect(g_ren,&rc);
+}
+
+static void draw_bar(int x, int y, int prog) {
+    int fillW = prog*(BAR_W-4)/100;
+    char pct[8];
+    fill(x,y,BAR_W,BAR_H,192,192,192);
+    outline(x,y,BAR_W,BAR_H,0,0,0);
+    if (fillW>0) fill(x+2,y+2,fillW,BAR_H-4,0,0,128);
+    sprintf(pct,"%d%%",prog);
+    if (fillW>BAR_W/2) rtxt(x+BAR_W/2-12,y+3,pct,255,255,255);
+    else               rtxt(x+BAR_W/2-12,y+3,pct,0,0,0);
+}
+
+static void draw_ghost(int x, int y, int idx, int total) {
+    int fade = 200-(idx*200/(total>1?total:1));
+    if (fade<20) fade=20;
+    fill(x,y,BAR_W,BAR_H,(Uint8)fade,(Uint8)fade,(Uint8)fade);
+}
+
+static void draw_dot(Dot *d) {
+    Col c;
+    const char *ch = NULL;
+    if (d->kind==DOT_RED) {
+        int fl=(g_animTick/4)%2;
+        c.r=fl?200:120; c.g=0; c.b=0;
+        ch="!";
+    } else if (d->kind==DOT_RANDOM) {
+        c=cycle_col(); ch="?";
+    } else {
+        c=dot_col(d->kind);
+        if (d->kind==DOT_GREY) ch="0";
+    }
+    fill(d->x,d->y,16,16,c.r,c.g,c.b);
+    outline(d->x,d->y,16,16,0,0,0);
+    if (ch) {
+        Uint8 t=(d->kind==DOT_GREY)?50:255;
+        rtxt(d->x+3,d->y+1,ch,t,t,t);
+    }
+}
+
+static void draw_bsod(void) {
+    fill(0,0,WIN_W,WIN_H,0,0,170);
+    rtxt(50,80, "A fatal exception 0E has occurred at 0028:C000034F in",255,255,255);
+    rtxt(50,98, "VXD VMM(01) + 00010E36. The current application will be",255,255,255);
+    rtxt(50,116,"terminated.",255,255,255);
+    rtxt(50,152,"*  Press any key to terminate the current application.",255,255,255);
+    rtxt(50,170,"*  Press CTRL+ALT+DEL again to restart your computer.",255,255,255);
+    rtxt(50,188,"   You will lose any unsaved information in all",255,255,255);
+    rtxt(50,206,"   applications.",255,255,255);
+    rtxt(50,250,"Press any key to continue _",255,255,255);
+}
+
+static void draw_gameover(void) {
+    char msg[128];
+    fill(0,0,WIN_W,WIN_H,0,0,0);
+    rtxt_lg(WIN_W/2-50,WIN_H/2-30,"GAME OVER",255,255,255);
+    sprintf(msg,"Score: %d   Level: %d",g_score,g_level);
+    rtxt(WIN_W/2-70,WIN_H/2+10,msg,255,255,255);
+    rtxt(WIN_W/2-90,WIN_H/2+40,"Press ENTER to play again",255,255,255);
+}
+
+static void render(void) {
+    int i;
+    char hud[256];
+
+    SDL_RenderClear(g_ren);
+
+    if (g_bsod)     { draw_bsod();     SDL_RenderPresent(g_ren); return; }
+    if (g_gameOver) { draw_gameover(); SDL_RenderPresent(g_ren); return; }
+
+    fill(0,0,WIN_W,AREA_H,0,128,128);
+
+    for (i=g_gCount-1; i>=0; i--) {
+        int idx=(g_gHead-1-i+MAX_GHOSTS)%MAX_GHOSTS;
+        draw_ghost(g_ghosts[idx].x,g_ghosts[idx].y,i,g_gCount);
+    }
+    for (i=0; i<NUM_DOTS; i++)
+        if (g_dots[i].alive) draw_dot(&g_dots[i]);
+    draw_bar(g_barX,g_barY,g_progress);
+
+    fill(0,AREA_H,WIN_W,22,192,192,192);
+    sprintf(hud,"Level: %d   Score: %d   Lives: %d   %s",
+        g_level,g_score,g_lives,g_perfectionist?"[Perfectionist]":"");
+    rtxt(6,AREA_H+3,hud,0,0,0);
+
+    SDL_RenderPresent(g_ren);
+}
+
+/* ---- font loading ---- */
+
+static TTF_Font *try_font(int sz) {
+    const char *paths[] = {
+        "/usr/share/fonts/truetype/dejavu/DejaVuSansMono.ttf",
+        "/usr/share/fonts/TTF/DejaVuSansMono.ttf",
+        "/usr/share/fonts/dejavu/DejaVuSansMono.ttf",
+        "/usr/share/fonts/truetype/liberation/LiberationMono-Regular.ttf",
+        "/System/Library/Fonts/Menlo.ttc",
+        "C:\\Windows\\Fonts\\cour.ttf",
+        NULL
+    };
+    int i; TTF_Font *f;
+    for (i=0; paths[i]; i++) { f=TTF_OpenFont(paths[i],sz); if(f) return f; }
+    return NULL;
+}
+
+/* ---- main ---- */
+
+int main(int argc, char *argv[]) {
+    SDL_Event e;
+    Uint32 last, now;
+    int running=1;
+    (void)argc; (void)argv;
+
+    SDL_Init(SDL_INIT_VIDEO);
+    TTF_Init();
+
+    g_win = SDL_CreateWindow("ProgressBar95",
+        SDL_WINDOWPOS_CENTERED,SDL_WINDOWPOS_CENTERED,
+        WIN_W,WIN_H,SDL_WINDOW_SHOWN);
+    g_ren = SDL_CreateRenderer(g_win,-1,
+        SDL_RENDERER_ACCELERATED|SDL_RENDERER_PRESENTVSYNC);
+
+    g_font  = try_font(13);
+    g_fontLg= try_font(28);
+
+    game_init();
+    last = SDL_GetTicks();
+
+    while (running) {
+        while (SDL_PollEvent(&e)) {
+            if (e.type==SDL_QUIT) { running=0; break; }
+
+            if (e.type==SDL_KEYDOWN) {
+                if (e.key.keysym.sym==SDLK_ESCAPE) { running=0; break; }
+                if (g_bsod) {
+                    game_dismiss_bsod();
+                } else if (g_gameOver && e.key.keysym.sym==SDLK_RETURN) {
+                    game_restart();
+                }
+            }
+
+            if (e.type==SDL_MOUSEBUTTONDOWN && e.button.button==SDL_BUTTON_LEFT) {
+                if (g_bsod) game_dismiss_bsod();
+                else game_click(e.button.x, e.button.y);
+            }
+            if (e.type==SDL_MOUSEBUTTONUP && e.button.button==SDL_BUTTON_LEFT) {
+                game_release();
+            }
+            if (e.type==SDL_MOUSEMOTION) {
+                game_drag(e.motion.x, e.motion.y);
+            }
+        }
+
+        now = SDL_GetTicks();
+        if (now-last >= (Uint32)TICK_MS) {
+            last=now;
+            if (!g_gameOver) game_tick();
+        }
+
+        render();
+    }
+
+    if (g_fontLg) TTF_CloseFont(g_fontLg);
+    if (g_font)   TTF_CloseFont(g_font);
+    SDL_DestroyRenderer(g_ren);
+    SDL_DestroyWindow(g_win);
+    TTF_Quit();
+    SDL_Quit();
+    return 0;
+}
---
+++ platform_win32.c
@@ -0,0 +1,298 @@
+/*
+ * platform_win32.c - Win32/GDI renderer for ProgressBar95
+ * Targets Windows 95/98/ME - pure Win32 API, no modern deps
+ *
+ * Compile: gcc -o progressbar95.exe platform_win32.c game.c -lgdi32 -luser32 -mwindows -std=c89
+ * Or: cl platform_win32.c game.c user32.lib gdi32.lib /subsystem:windows
+ */
+
+#define WIN32_LEAN_AND_MEAN
+#include <windows.h>
+#include <stdio.h>
+#include <string.h>
+#include "game.h"
+
+#define TIMER_ID  1
+#define TIMER_MS  40
+#define WIN_W     (AREA_W + 16)
+#define WIN_H     (AREA_H + 58)
+
+static HWND g_hwnd;
+
+/* ---- rendering helpers ---- */
+
+static void draw_bar(HDC hdc, int x, int y, int prog) {
+    RECT r;
+    HBRUSH br;
+    HPEN pen, oldPen;
+    HBRUSH oldBr;
+    int fillW = prog * (BAR_W - 4) / 100;
+    char pct[8];
+
+    pen = CreatePen(PS_SOLID, 1, RGB(0,0,0));
+    oldPen = (HPEN)SelectObject(hdc, pen);
+    br = CreateSolidBrush(RGB(192,192,192));
+    oldBr = (HBRUSH)SelectObject(hdc, br);
+    Rectangle(hdc, x, y, x+BAR_W, y+BAR_H);
+    SelectObject(hdc, oldBr); SelectObject(hdc, oldPen);
+    DeleteObject(br); DeleteObject(pen);
+
+    if (fillW > 0) {
+        br = CreateSolidBrush(RGB(0,0,128));
+        r.left = x+2; r.top = y+2; r.right = x+2+fillW; r.bottom = y+BAR_H-2;
+        FillRect(hdc, &r, br);
+        DeleteObject(br);
+    }
+
+    sprintf(pct, "%d%%", prog);
+    SetBkMode(hdc, TRANSPARENT);
+    SetTextColor(hdc, fillW > BAR_W/2 ? RGB(255,255,255) : RGB(0,0,0));
+    TextOut(hdc, x+BAR_W/2-12, y+3, pct, lstrlen(pct));
+}
+
+static void draw_ghost(HDC hdc, int x, int y, int idx, int total) {
+    HPEN pen, oldPen;
+    HBRUSH br, oldBr;
+    int fade = 200 - (idx * 200 / (total > 1 ? total : 1));
+    COLORREF c;
+    if (fade < 20) fade = 20;
+    c = RGB(fade,fade,fade);
+    pen = CreatePen(PS_SOLID,1,c); oldPen = (HPEN)SelectObject(hdc,pen);
+    br = CreateSolidBrush(c);     oldBr  = (HBRUSH)SelectObject(hdc,br);
+    Rectangle(hdc, x, y, x+BAR_W, y+BAR_H);
+    SelectObject(hdc,oldBr); SelectObject(hdc,oldPen);
+    DeleteObject(br); DeleteObject(pen);
+}
+
+static COLORREF dot_color(DotKind k) {
+    switch (k) {
+        case DOT_BLUE:   return RGB(0,80,220);
+        case DOT_ORANGE: return RGB(255,140,0);
+        case DOT_PINK:   return RGB(255,50,150);
+        case DOT_GREY:   return RGB(150,150,150);
+        case DOT_RED:    return RGB(200,0,0);
+        case DOT_RANDOM: return RGB(180,0,200);
+        case DOT_GREEN:  return RGB(0,180,0);
+        default:         return RGB(255,255,255);
+    }
+}
+
+static COLORREF cycle_color(void) {
+    static COLORREF c[] = {
+        RGB(0,80,220),RGB(255,140,0),RGB(255,50,150),
+        RGB(150,150,150),RGB(200,0,0),RGB(0,180,0)
+    };
+    return c[(g_animTick/6)%6];
+}
+
+static void draw_dot(HDC hdc, Dot *d) {
+    RECT r;
+    HBRUSH br;
+    COLORREF bg;
+    const char *ch = NULL;
+
+    if (d->kind == DOT_RED) {
+        bg = (g_animTick/4)%2 ? RGB(200,0,0) : RGB(120,0,0);
+        ch = "!";
+    } else if (d->kind == DOT_RANDOM) {
+        bg = cycle_color();
+        ch = "?";
+    } else {
+        bg = dot_color(d->kind);
+        if (d->kind == DOT_GREY) ch = "0";
+    }
+
+    br = CreateSolidBrush(bg);
+    r.left=d->x; r.top=d->y; r.right=d->x+16; r.bottom=d->y+16;
+    FillRect(hdc, &r, br); DeleteObject(br);
+    br = CreateSolidBrush(RGB(0,0,0));
+    FrameRect(hdc, &r, br); DeleteObject(br);
+
+    if (ch) {
+        BYTE t = (d->kind==DOT_GREY) ? 50 : 255;
+        SetBkMode(hdc, TRANSPARENT);
+        SetTextColor(hdc, RGB(t,t,t));
+        TextOut(hdc, d->x+3, d->y+1, ch, 1);
+    }
+}
+
+static void draw_bsod(HDC hdc) {
+    RECT r;
+    HBRUSH br;
+    HFONT font, oldFont;
+
+    br = CreateSolidBrush(RGB(0,0,170));
+    r.left=0; r.top=0; r.right=AREA_W; r.bottom=AREA_H+20;
+    FillRect(hdc,&r,br); DeleteObject(br);
+
+    font = CreateFont(16,0,0,0,FW_NORMAL,FALSE,FALSE,FALSE,
+        ANSI_CHARSET,OUT_DEFAULT_PRECIS,CLIP_DEFAULT_PRECIS,
+        DEFAULT_QUALITY,FIXED_PITCH|FF_MODERN,"Courier New");
+    oldFont = (HFONT)SelectObject(hdc, font);
+    SetTextColor(hdc, RGB(255,255,255));
+    SetBkColor(hdc, RGB(0,0,170));
+    SetBkMode(hdc, OPAQUE);
+
+    TextOut(hdc,50,80, "A fatal exception 0E has occurred at 0028:C000034F in",53);
+    TextOut(hdc,50,98, "VXD VMM(01) + 00010E36. The current application will be",55);
+    TextOut(hdc,50,116,"terminated.",11);
+    TextOut(hdc,50,152,"*  Press any key to terminate the current application.",54);
+    TextOut(hdc,50,170,"*  Press CTRL+ALT+DEL again to restart your computer.",53);
+    TextOut(hdc,50,188,"   You will lose any unsaved information in all",47);
+    TextOut(hdc,50,206,"   applications.",16);
+    TextOut(hdc,50,250,"Press any key to continue _",27);
+
+    SelectObject(hdc, oldFont); DeleteObject(font);
+}
+
+static void draw_gameover(HDC hdc) {
+    RECT r;
+    HBRUSH br;
+    char msg[128];
+
+    br = CreateSolidBrush(RGB(0,0,0));
+    r.left=0; r.top=0; r.right=AREA_W; r.bottom=AREA_H+20;
+    FillRect(hdc,&r,br); DeleteObject(br);
+
+    SetTextColor(hdc,RGB(255,255,255));
+    SetBkMode(hdc,OPAQUE); SetBkColor(hdc,RGB(0,0,0));
+    TextOut(hdc,AREA_W/2-50,AREA_H/2-20,"GAME OVER",9);
+    sprintf(msg,"Score: %d  Level: %d",g_score,g_level);
+    TextOut(hdc,AREA_W/2-60,AREA_H/2+10,msg,lstrlen(msg));
+    TextOut(hdc,AREA_W/2-70,AREA_H/2+40,"Press ENTER to play again",25);
+}
+
+static void on_paint(HWND hwnd) {
+    PAINTSTRUCT ps;
+    HDC hdc, memDC;
+    HBITMAP bmp, oldBmp;
+    RECT clientR;
+    HBRUSH bg;
+    char hud[256];
+    int i;
+
+    hdc = BeginPaint(hwnd, &ps);
+    GetClientRect(hwnd, &clientR);
+    memDC = CreateCompatibleDC(hdc);
+    bmp = CreateCompatibleBitmap(hdc, clientR.right, clientR.bottom);
+    oldBmp = (HBITMAP)SelectObject(memDC, bmp);
+
+    if (g_bsod) {
+        draw_bsod(memDC);
+    } else if (g_gameOver) {
+        draw_gameover(memDC);
+    } else {
+        bg = CreateSolidBrush(RGB(0,128,128));
+        FillRect(memDC, &clientR, bg); DeleteObject(bg);
+
+        for (i = g_gCount-1; i >= 0; i--) {
+            int idx = (g_gHead-1-i+MAX_GHOSTS)%MAX_GHOSTS;
+            draw_ghost(memDC, g_ghosts[idx].x, g_ghosts[idx].y, i, g_gCount);
+        }
+        for (i = 0; i < NUM_DOTS; i++)
+            if (g_dots[i].alive) draw_dot(memDC, &g_dots[i]);
+        draw_bar(memDC, g_barX, g_barY, g_progress);
+
+        bg = CreateSolidBrush(RGB(192,192,192));
+        clientR.top = clientR.bottom - 22;
+        FillRect(memDC, &clientR, bg); DeleteObject(bg);
+
+        sprintf(hud, "Level: %d   Score: %d   Lives: %d   %s",
+            g_level, g_score, g_lives,
+            g_perfectionist ? "[Perfectionist]" : "");
+        SetBkColor(memDC,RGB(192,192,192));
+        SetTextColor(memDC,RGB(0,0,0));
+        SetBkMode(memDC,OPAQUE);
+        TextOut(memDC, 6, clientR.top+3, hud, lstrlen(hud));
+    }
+
+    GetClientRect(hwnd, &clientR);
+    BitBlt(hdc,0,0,clientR.right,clientR.bottom,memDC,0,0,SRCCOPY);
+    SelectObject(memDC,oldBmp); DeleteObject(bmp); DeleteDC(memDC);
+    EndPaint(hwnd, &ps);
+}
+
+/* ---- window proc ---- */
+
+LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) {
+    switch (msg) {
+        case WM_CREATE:
+            game_init();
+            SetTimer(hwnd, TIMER_ID, TIMER_MS, NULL);
+            return 0;
+
+        case WM_TIMER:
+            if (!g_gameOver) game_tick();
+            InvalidateRect(hwnd, NULL, FALSE);
+            return 0;
+
+        case WM_LBUTTONDOWN:
+            if (g_bsod) {
+                game_dismiss_bsod();
+            } else if (g_gameOver) {
+                /* nothing on click */
+            } else {
+                game_click(LOWORD(lParam), HIWORD(lParam));
+                SetCapture(hwnd);
+            }
+            return 0;
+
+        case WM_MOUSEMOVE:
+            game_drag(LOWORD(lParam), HIWORD(lParam));
+            return 0;
+
+        case WM_LBUTTONUP:
+            game_release();
+            ReleaseCapture();
+            return 0;
+
+        case WM_KEYDOWN:
+            if (g_bsod) {
+                game_dismiss_bsod();
+            } else if (g_gameOver && wParam == VK_RETURN) {
+                game_restart();
+            }
+            InvalidateRect(hwnd, NULL, FALSE);
+            return 0;
+
+        case WM_PAINT:
+            on_paint(hwnd);
+            return 0;
+
+        case WM_DESTROY:
+            KillTimer(hwnd, TIMER_ID);
+            PostQuitMessage(0);
+            return 0;
+    }
+    return DefWindowProc(hwnd, msg, wParam, lParam);
+}
+
+int WINAPI WinMain(HINSTANCE hInst, HINSTANCE hPrev, LPSTR lpCmd, int nShow) {
+    WNDCLASS wc;
+    MSG msg;
+    (void)hPrev; (void)lpCmd;
+
+    memset(&wc, 0, sizeof(wc));
+    wc.style         = CS_HREDRAW|CS_VREDRAW;
+    wc.lpfnWndProc   = WndProc;
+    wc.hInstance     = hInst;
+    wc.hbrBackground = (HBRUSH)(COLOR_WINDOW+1);
+    wc.lpszClassName = "ProgressBar95";
+    wc.hCursor       = LoadCursor(NULL, IDC_ARROW);
+    wc.hIcon         = LoadIcon(NULL, IDI_APPLICATION);
+    RegisterClass(&wc);
+
+    g_hwnd = CreateWindow("ProgressBar95","ProgressBar95",
+        WS_OVERLAPPEDWINDOW|WS_VISIBLE,
+        80,60,WIN_W,WIN_H,NULL,NULL,hInst,NULL);
+    if (!g_hwnd) return 1;
+
+    ShowWindow(g_hwnd, nShow);
+    UpdateWindow(g_hwnd);
+
+    while (GetMessage(&msg,NULL,0,0)) {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
+    return (int)msg.wParam;
+}
